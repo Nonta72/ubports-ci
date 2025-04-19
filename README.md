851165cb503191f5e882bba21878b421326a2742
jsbsbxjxh66 <106220792+wu17481748@users.noreply.github.com>
2025-04-19 16:10:14 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# ubports-ci
+# UBports GH Actions
+This repository enables images to be built from GitHub Actions.
 
-xiaomi-cannon halium-12.0
+
+![](https://github.com/ubuntu-touch-clover/ubports-ci/workflows/Build%20System%20Image/badge.svg)
+
+**To download files from CI, please refer to the figures below:**
+
+![](https://github.com/ubports-lavender/ubports-ci/raw/master/images/Screenshot_20200114_024916.png)
---
+++ build-hal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source halium.env
+cd $ANDROID_ROOT
+
+
+source build/envsetup.sh
+bash hybris-patches/apply-patches.sh --mb
+export USE_CCACHE=1
+breakfast $DEVICE
+#make -j$(nproc) mkbootimg
+#make -j$(nproc) fec
+#make -j$(nproc) halium-boot
+make -j$(nproc) systemimage 
+#make -j$(nproc) vendorimage
+
+echo "md5sum halium-boot.img system.img vendor.img"
+#md5sum $ANDROID_ROOT/out/target/product/clover/halium-boot.img
+md5sum $ANDROID_ROOT/out/target/product/clover/system.img
+#md5sum $ANDROID_ROOT/out/target/product/clover/vendor.img
